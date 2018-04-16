9a1161bfa2180b77d0c9fabbd4db6c5f2039f89b
Shuhao Chen <shuhao.chen@sjsu.edu>
2018-04-16 14:38:21 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
+const morgan = require('morgan');
+const router = require('./router')
+
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*'}));
+router(app);
+
+
+const port = process.env.PORT || 8080;
+const server = http.createServer(app);
+server.listen(port);
+console.log('server start',port);
